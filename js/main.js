3cdc8b123fe39257811eed8c3dfb301b0676af13
Alexander K <niktonezapomnil@gmail.com>
2019-11-27 12:29:44 +0500

    init fixes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 $(document).ready(function(){
-    console.log('yo');
-
-    $('.line-dec').css('background-size', $(window).width())
+    $('.line-dec').css('background-size', $(window).width());
     $(window).resize(function(e){
-        console.log();
-
-        $('.line-dec').css('background-size', $(window).width())
-    })
-})
+        $('.line-dec').css('background-size', $(window).width());
+    });
+    if($(window).innerWidth() > 1000) {
+    	$('.promo__left').mouseover(function(){
+	    	$('.choice').removeClass('left right');
+	    	$('.choice').addClass('right');
+	    	$(this).removeClass('blur');
+	    	$(this).siblings().addClass('blur');
+	    });
+	    $('.promo__right').mouseover(function(){
+	    	$('.choice').removeClass('left right');
+	    	$('.choice').addClass('left');
+	    	$(this).removeClass('blur');
+	    	$(this).siblings().addClass('blur');
+	    });
+	    $('.promo__left, .promo__right').mouseout(function(){
+	    	$('.choice').removeClass('left right');
+	    	$('.promo__left, .promo__right').removeClass('blur');
+	    });
+    }
+});
